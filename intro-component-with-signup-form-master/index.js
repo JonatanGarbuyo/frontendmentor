0221b1850bf902efd15ee5c6d0f646bcfcfa7500
Jonatan Garbuyo <jonatangarbuyo@gmail.com>
2023-02-10 09:56:45 -0300

    change input styles on error

--- intro-component-with-signup-form-master/index.js
+++ intro-component-with-signup-form-master/index.js
@@ -11,32 +11,32 @@ function validate(event) {
   const element = event.target
 
   if (isEmpty(element) && element.type !== 'email') {
+    element.parentElement.children[0].classList.add('error')
     element.parentElement.children['err-icon'].classList.remove('hidden-error')
     element.parentElement.children['err-msg'].classList.remove('hidden-error')
   }
   if (!isEmpty(element) && element.type !== 'email') {
+    element.parentElement.children[0].classList.remove('error')
     element.parentElement.children['err-icon'].classList.add('hidden-error')
     element.parentElement.children['err-msg'].classList.add('hidden-error')
   }
 
   if (!validEmail(element) && element.type == 'email') {
+    element.parentElement.children[0].classList.add('error')
     element.parentElement.children['err-icon'].classList.remove('hidden-error')
     element.parentElement.children['err-msg'].classList.remove('hidden-error')
   }
   if (validEmail(element) && element.type == 'email') {
+    element.parentElement.children[0].classList.remove('error')
     element.parentElement.children['err-icon'].classList.add('hidden-error')
     element.parentElement.children['err-msg'].classList.add('hidden-error')
   }
 }
 
 function isEmpty(element) {
-  return element.value.length < 1
+  return element.value.trim().length < 1
 }
 
 function validEmail(element) {
   return element.validity.valid
 }
-
-/*<span class="error error-msg err-msg-email">
-              Looks like this is not an email
-            </span>*/
