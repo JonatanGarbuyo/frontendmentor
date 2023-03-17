da342d05cfd2519bdbed6ac5957a7485d3fcb22f
Jonatan Garbuyo <jonatangarbuyo@gmail.com>
2023-03-16 23:00:24 -0300

    add form validation

--- manage-landing-page-master/index.js
+++ manage-landing-page-master/index.js
@@ -1,3 +1,4 @@
+/* Slider */
 document.querySelectorAll('.carousel-control-input').forEach((control) => {
   control.addEventListener('click', (e) => {
     const cardId = e.target.dataset.cardId
@@ -14,14 +15,14 @@ const observer = new IntersectionObserver(checkCarouselControl, {
   rootMargin: '0px',
   threshold: 1.0,
   trackVisibility: true,
-  delay: 100,
+  delay: 200,
 })
 
 document.querySelectorAll('.carousel-card').forEach((card) => {
   observer.observe(card)
 })
 
-function checkCarouselControl(entries /*observer*/) {
+function checkCarouselControl(entries) {
   entries.forEach((entry) => {
     const id = entry.target.id
     const control = document.querySelector(`input[data-card-id=${id}]`)
@@ -30,3 +31,21 @@ function checkCarouselControl(entries /*observer*/) {
     }
   })
 }
+
+/* Form */
+const inputEmail = document.querySelector('input[type=email]')
+const errorMessage = document.querySelector('.error-message')
+
+document.querySelector('form').addEventListener('submit', (e) => {
+  e.preventDefault()
+  alert('Email submited, thank you')
+  e.target.reset()
+})
+
+inputEmail.addEventListener('blur', (e) => {
+  if (e.target.validity.valid) {
+    errorMessage.classList.remove('error-visible')
+  } else {
+    errorMessage.classList.add('error-visible')
+  }
+})
